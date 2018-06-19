a3dd579ef56b27c4a44f3755edff2f59c1013ea5
alice <rainingilove@gmail.com>
2018-06-19 16:48:40 +0200

    test mint

--- tests/test_coin.py
+++ tests/test_coin.py
@@ -3,5 +3,14 @@ def test_minter(chain):
     assert "0x82A978B3f5962A5b0957d9ee9eEf472EE55B42F1" == coin.call().minter()
 
 
-def test_mint(chain):
-    pass
+def test_mint(chain, accounts):
+    coin, _ = chain.provider.get_or_deploy_contract('Coin')
+    account = accounts[1]
+    assert coin.call().minter() != account
+    txn_hash = coin.transact().mint(account, 21)
+    chain.wait.for_receipt(txn_hash)
+    assert 21 == coin.call().balances(account)
+
+    txn_hash = coin.transact().mint(account, 30)
+    chain.wait.for_receipt(txn_hash)
+    assert 51 == coin.call().balances(account)
